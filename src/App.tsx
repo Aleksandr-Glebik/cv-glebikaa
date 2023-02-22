6a33bf2878f9e482dfd5adc5ba2439bfe5745efb
Aleksandr-Glebik <glebikaa@mail.ru>
2023-02-22 16:53:24 +0300

    init: start project, i sass, add @types includes, add sass normalize, variable, app; link google fonts

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './scss/app.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="job">Frontend Developer React</p>
+      <p className="name">Aleksandr Glebik</p>
+    </div>
+  );
+}
+
+export default App;
