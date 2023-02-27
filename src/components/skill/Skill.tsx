10ac135abaf33e667120c0441d0051022f8501de
Aleksandr-Glebik <glebikaa@mail.ru>
2023-02-27 15:55:43 +0300

    feat: i fream-motion, add animation for comp SkillsComp and Skill

--- src/components/skill/Skill.tsx
+++ src/components/skill/Skill.tsx
@@ -1,22 +1,34 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import styles from './Skill.module.scss'
 import clsx from 'clsx'
+import { motion } from 'framer-motion'
 
 type skillPropsType = {
     text: string
     src: string
     id: number
+    custom: number
+    variants: any
 }
 
-const Skill: React.FC<skillPropsType> = ({ text, src, id}) => {
+type Ref = HTMLDivElement | null
+
+const Skill: React.FC<skillPropsType> = forwardRef<Ref, skillPropsType>(({ text, src, id, custom, variants}, ref) => {
     return (
-    <div className={styles.skill}>
+    <motion.div
+      className={styles.skill}
+      ref={ref}
+      custom={custom}
+      variants={variants}
+    >
       <img src={src} alt="skill" className={clsx(styles.skill__icon, {
         [styles.small]: id === 0 || id === 2 || id === 5 || id === 6
       })}/>
       <p className={styles.skill__text}>{text}</p>
-    </div>
+    </motion.div>
   )
-}
+})
 
 export default Skill
+
+export const MSkill = motion(Skill)
--- src/pages/Skills/SkillsComp.tsx
+++ src/pages/Skills/SkillsComp.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './SkillsComp.module.scss'
+import { motion } from 'framer-motion'
 
 import skillsAstronaut from '../../assets/img/astronautCase.png'
 import reactSrc from '../../assets/icon/react.png'
@@ -13,7 +14,7 @@ import webPackSrc from '../../assets/icon/webpack.png'
 import gitSrc from '../../assets/icon/githubWhite.png'
 import figmaSrc from '../../assets/icon/figma.png'
 
-import Skill from '../../components/skill/Skill'
+import MSkill from '../../components/skill/Skill'
 
 type skillItemType = {
   id: number
@@ -85,6 +86,40 @@ const skillsList: skillItemType[] = [
   },
 ]
 
+const imgAnimation = {
+  hidden: {
+    x: -200,
+    opacity: 0,
+  },
+  visible: (custom: number)  => ({
+    x: 0,
+    opacity: 1,
+    transition: { delay: custom * 0.2}
+  }),
+}
+const contentAnimation = {
+  hidden: {
+    x: 300,
+    opacity: 0,
+  },
+  visible: (custom: number)  => ({
+    x: 0,
+    opacity: 1,
+    transition: { delay: custom * 0.2}
+  }),
+}
+
+const itemAnimation = {
+  hidden: {
+    x: 200,
+    opacity: 0,
+  },
+  visible: (custom: number) => ({
+    x: 0,
+    opacity: 1,
+    transition: { delay: custom * 0.2}
+  })
+}
 
 const SkillsComp: React.FC = () => {
   return (
@@ -92,17 +127,46 @@ const SkillsComp: React.FC = () => {
       className={styles.wrapper}
     >
       <div className={styles.parallax}>
-        <div className={styles.container}>
-          <img src={skillsAstronaut} alt="astronaut" className={styles.astronaut}/>
+        <motion.section
+          initial="hidden"
+          whileInView="visible"
+          viewport={{
+            amount: 0.2,
+            // once: true
+          }}
+          className={styles.container}
+        >
+          <motion.img
+            src={skillsAstronaut}
+            alt="astronaut"
+            className={styles.astronaut}
+            custom={2}
+            variants={imgAnimation}
+          />
           <div className={styles.content}>
-            <h2 className={styles.content_title}>Skills:</h2>
-            <div className={styles.content_items}>
-              {skillsList.map( (item) => (
-                <Skill key={item.id} text={item.text} src={item.path} id={Number(item.id)} />
+            <motion.h2
+              className={styles.content_title}
+              custom={1}
+              variants={contentAnimation}
+            >Skills:</motion.h2>
+            <motion.div
+              custom={2}
+              variants={contentAnimation}
+              className={styles.content_items}
+            >
+              {skillsList.map( (item, ind) => (
+                <MSkill
+                  key={`${item.id}_${ind}`}
+                  text={item.text}
+                  src={item.path}
+                  id={Number(item.id)}
+                  custom={ind + 1}
+                  variants={itemAnimation}
+                />
               ))}
-            </div>
+            </motion.div>
           </div>
-        </div>
+        </motion.section>
       </div>
     </div>
   )
