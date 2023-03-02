21238fdaead364ef3a6ee897f7e051f8edf3b988
Aleksandr-Glebik <glebikaa@mail.ru>
2023-03-02 23:32:59 +0300

    feat: add media query for page Home (1201 and higher)

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import Home from './pages/Home/Home';
-import SkillsComp from './pages/Skills/SkillsComp';
-import About from './pages/About/About';
-import Contacts from './pages/Contacts/Contacts';
-import Projects from './pages/Projects/Projects';
-import Languages from './pages/Languages/Languages';
+// import SkillsComp from './pages/Skills/SkillsComp';
+// import About from './pages/About/About';
+// import Contacts from './pages/Contacts/Contacts';
+// import Projects from './pages/Projects/Projects';
+// import Languages from './pages/Languages/Languages';
 import './scss/app.scss';
 
 const App: React.FC = () => {
   return (
     <div className='wrapper'>
       <Home />
-      <SkillsComp />
-      <About />
-      <Contacts />
-      <Projects />
-      <Languages />
+      {/* <SkillsComp /> */}
+      {/* <About /> */}
+      {/* <Contacts /> */}
+      {/* <Projects /> */}
+      {/* <Languages /> */}
     </div>
   );
 }
