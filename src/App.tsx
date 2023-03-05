a0e595fe4042137a65f6edfc6baeecb6d0d99988
Aleksandr-Glebik <glebikaa@mail.ru>
2023-03-06 01:18:03 +0300

    feat: add adaptive for page contacts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // import Home from './pages/Home/Home';
 // import SkillsComp from './pages/Skills/SkillsComp';
-import About from './pages/About/About';
-// import Contacts from './pages/Contacts/Contacts';
+// import About from './pages/About/About';
+import Contacts from './pages/Contacts/Contacts';
 // import Projects from './pages/Projects/Projects';
 // import Languages from './pages/Languages/Languages';
 import './scss/app.scss';
@@ -12,8 +12,8 @@ const App: React.FC = () => {
     <div className='wrapper'>
       {/* <Home /> */}
       {/* <SkillsComp /> */}
-      <About />
-      {/* <Contacts /> */}
+      {/* <About /> */}
+      <Contacts />
       {/* <Projects /> */}
       {/* <Languages /> */}
     </div>
