699b8d3ec0faab169dce5cf60cf63a828ff9ed94
Aleksandr-Glebik <glebikaa@mail.ru>
2023-08-08 11:44:04 +0300

    feat: add new projects, rename old projects

--- src/data/ProjectsData.tsx
+++ src/data/ProjectsData.tsx
@@ -6,8 +6,10 @@ import htmlSrc from '../assets/icon/html5.png'
 import cssSrc from '../assets/icon/css.png'
 import scssSrc from '../assets/icon/sass.png'
 import portfolioSrc from '../assets/img/portfolio.jpg'
-import todoListSrc from '../assets/img/todoList.jpg'
-import OnlineStoreSrc from '../assets/img/onlineStore.jpg'
+// import todoListSrc from '../assets/img/todoList.jpg'
+import onlineStoreSrc from '../assets/img/onlineStore.jpg'
+import tableManager from '../assets/img/table.jpg'
+import quizCreator from '../assets/img/quiz.jpg'
 
 type projectsType = {
     id: number
@@ -22,25 +24,41 @@ export const projectsList: projectsType[] = [
     {
         id: 0,
         name: 'portfolio',
-        title: 'Portfolio',
+        title: 'Website',
         projectLink: 'https://aleksandr-glebik.github.io/portfolio-JSFEPRESCHOOL/portfolio/',
         projectImg: portfolioSrc,
         technologies: [htmlSrc, cssSrc, jsSrc]
     },
     {
         id: 1,
-        name: 'todoList',
-        title: 'Todo List',
-        projectLink: 'https://aleksandr-glebik.github.io/text-editor/',
-        projectImg: todoListSrc,
+        name: 'quizCreator',
+        title: 'Quiz Creator',
+        projectLink: 'https://aleksandr-glebik.github.io/react-quiz-fc/',
+        projectImg: quizCreator,
         technologies: [reactSrc, reduxSrc, typeScriptSrc, htmlSrc, scssSrc]
     },
+    // {
+    //     id: 1,
+    //     name: 'todoList',
+    //     title: 'Task Manager',
+    //     projectLink: 'https://aleksandr-glebik.github.io/text-editor/',
+    //     projectImg: todoListSrc,
+    //     technologies: [reactSrc, reduxSrc, typeScriptSrc, htmlSrc, scssSrc]
+    // },
     {
         id: 2,
         name: 'onlineStore',
         title: 'Online Store',
         projectLink:  'https://Aleksandr-Glebik.github.io/react-pizza-v2',
-        projectImg: OnlineStoreSrc,
+        projectImg: onlineStoreSrc,
+        technologies: [reactSrc, reduxSrc, typeScriptSrc, htmlSrc, scssSrc]
+    },
+    {
+        id: 3,
+        name: 'tableManager',
+        title: 'Table',
+        projectLink:  'https://aleksandr-glebik.github.io/sector-business-test-task/',
+        projectImg: tableManager,
         technologies: [reactSrc, reduxSrc, typeScriptSrc, htmlSrc, scssSrc]
     },
 ]
